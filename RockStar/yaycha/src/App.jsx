3335f76d7f5c402e1b1ce8140b337dc21fa9b5bc
codingsake <56745055+thinthazin96@users.noreply.github.com>
2024-08-21 14:49:31 -0400

    Updated the code using State Data

--- RockStar/yaycha/src/App.jsx
+++ RockStar/yaycha/src/App.jsx
@@ -1,14 +1,24 @@
+import { useState } from "react";
+
 import List from "./List.jsx";
 import Item from "./Item";
 
 export default function App(){
+  const [data, setData] = useState([
+    { id:1, content: "Hello, World!", name: "Alice"},
+    { id:1, content: "React is fun.", name: "Bob"},
+    { id:1, content: "Yay, interesting.", name: "Chris"},
+    
+  ]);
+
   return (
     <div style={{ maxWidth: 600, margin: "20px auto" }}>
       <h1>Yaycha</h1>
       <List>
-        <Item content="Hello, World!" name= "Alice" />
-        <Item content="React is fun." name= "Bob" />
+        {data.map(item => {
+          return <Item content={item.content} name={item.name} />
+        })}
       </List>
     </div>
-  )
+  );
 }
