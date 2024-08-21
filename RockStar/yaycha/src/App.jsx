601d1a2764718de8d358c72cc25c5bc03f9efcdd
codingsake <56745055+thinthazin96@users.noreply.github.com>
2024-08-21 14:41:53 -0400

    Create a file for Item and List component and link with App.jsx

--- RockStar/yaycha/src/App.jsx
+++ RockStar/yaycha/src/App.jsx
@@ -1,35 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import List from "./List.jsx";
+import Item from "./Item";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App(){
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div style={{ maxWidth: 600, margin: "20px auto" }}>
+      <h1>Yaycha</h1>
+      <List>
+        <Item content="Hello, World!" name= "Alice" />
+        <Item content="React is fun." name= "Bob" />
+      </List>
+    </div>
   )
 }
-
-export default App
