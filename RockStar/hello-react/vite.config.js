f9ca3b31df125a9cf6da08384fe341076e390141
codingsake <56745055+thinthazin96@users.noreply.github.com>
2024-08-21 14:00:10 -0400

    Updated Vite.config.js

--- RockStar/hello-react/vite.config.js
+++ RockStar/hello-react/vite.config.js
@@ -2,6 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+
+//Server section will auto open the page on browser on 'npm run dev' on port 3000
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000,
+    open: true,
+  }
 })
